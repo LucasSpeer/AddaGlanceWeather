d4074a7007d89f380209ec221bd1ae4746dc4c67
lucasspeer <lucasspeer123@gmail.com>
2024-06-04 14:08:39 -0500

    Basic weather card

--- app/src/main/java/com/lucasspeer/addaglanceweather/MainActivity.kt
+++ app/src/main/java/com/lucasspeer/addaglanceweather/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.lucasspeer.addaglanceweather.ui.theme.AddaGlanceWeatherTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             AddaGlanceWeatherTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                    )
+                    CurrentWeatherCard(modifier = Modifier
+                        .padding(innerPadding)
+                        .padding(horizontal = 16.dp))
                 }
             }
         }
@@ -31,17 +46,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AddaGlanceWeatherTheme {
-        Greeting("Android")
+fun CurrentWeatherCard(modifier: Modifier = Modifier) {
+    // Add padding around our message
+    Column( modifier =  modifier.background(color = MaterialTheme.colorScheme.secondary, shape = RoundedCornerShape(8.dp))
+        .height(height = 150.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier.fillMaxWidth().fillMaxHeight()
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.outline_wb_sunny_100),
+                contentDescription = "Current Weather Icon", //TODO add weather
+                Modifier.size(42.dp)
+            )
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+            Column (modifier = Modifier.fillMaxHeight()){
+                Text(text = "Sunny", color = MaterialTheme.colorScheme.primary, fontSize = 24.sp, fontFamily = FontFamily.Default, fontWeight = FontWeight.Bold)
+                // Add a vertical space between the author and message texts
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(text = "84°F", color = MaterialTheme.colorScheme.primary)
+            }
+        }
     }
 }
--- app/src/main/java/com/lucasspeer/addaglanceweather/ui/theme/Theme.kt
+++ app/src/main/java/com/lucasspeer/addaglanceweather/ui/theme/Theme.kt
@@ -9,18 +9,23 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+        primary = Color(0xFFEADEDA),
+        secondary = Color(0xFF63B4D1),
+        tertiary = Pink80,
+        background = Color(0xFFEADEDA),
 )
 
 private val LightColorScheme = lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
+        primary = Color(0xFFEADEDA),
+        secondary = Color(0xFF63B4D1),
+        tertiary = Pink40,
+        background = Color(0xFFEADEDA),
+        onBackground = Color(0xFFEADEDA),
+
 
         /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -35,17 +40,12 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun AddaGlanceWeatherTheme(
-        darkTheme: Boolean = isSystemInDarkTheme(),
+        darkTheme: Boolean = false,
         // Dynamic color is available on Android 12+
-        dynamicColor: Boolean = true,
+        dynamicColor: Boolean = false,
         content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
